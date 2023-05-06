9f09a2de56ac8b8ceba1e1e77f5802df396fbc3a
whtsht <whiteshirt0079@gmail.com>
2023-05-06 11:05:07 +0900

    web: LINEログインを追加

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,27 +1,69 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Hello } from "./secret";
+import liff from "@line/liff";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import "@fontsource/roboto/400.css";
+
+const color = { line: "#00D816" };
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React {Hello}
-                </a>
-            </header>
-        </div>
-    );
+    let isLoggedIn = false;
+    try {
+        isLoggedIn = liff.isLoggedIn();
+    } catch (e) {
+        console.log(e);
+    }
+    if (isLoggedIn) {
+        const token = liff.getDecodedIDToken();
+        return (
+            <Grid
+                container
+                direction="column"
+                alignItems="center"
+                justifyContent="space-evenly"
+                style={{ minHeight: "100vh" }}
+            >
+                <Grid item>
+                    <h1 style={{ textAlign: "center" }}>
+                        こんにちは {token?.name} さん
+                    </h1>
+                </Grid>
+                <Grid item>
+                    <Button
+                        variant="contained"
+                        sx={{ fontSize: 20, fontWeight: "bold" }}
+                        onClick={() => {
+                            liff.logout();
+                            window.location.reload();
+                        }}
+                    >
+                        ログアウト
+                    </Button>
+                </Grid>
+            </Grid>
+        );
+    } else {
+        return (
+            <Grid
+                container
+                spacing={3}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                style={{ minHeight: "100vh" }}
+            >
+                <Grid item>
+                    <Button
+                        variant="contained"
+                        style={{ backgroundColor: color.line }}
+                        sx={{ fontSize: 20, fontWeight: "bold" }}
+                        onClick={() => liff.login({})}
+                    >
+                        Lineアカウントでログイン
+                    </Button>
+                </Grid>
+            </Grid>
+        );
+    }
 }
 
 export default App;
