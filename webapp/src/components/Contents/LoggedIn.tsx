3a744d0b662c3a024a4ff63019d8db90ae32d8a9
whtsht <whiteshirt0079@gmail.com>
2023-05-07 11:26:00 +0900

    web: カレンダーを追加

--- webapp/src/components/Contents/LoggedIn.tsx
+++ webapp/src/components/Contents/LoggedIn.tsx
@@ -1,4 +1,56 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import allLocales from "@fullcalendar/core/locales-all";
+import interactionPlugin from "@fullcalendar/interaction";
+
 function LoggedIn() {
-    return <></>;
+    const initialView = "dayGridMonth";
+    const INITIAL_EVENTS = [
+        {
+            id: "id",
+            title: "my event",
+            start: "2023-05-10",
+            color: "#000",
+        },
+    ];
+
+    return (
+        <div
+            style={{
+                width: "100vw",
+                alignItems: "center",
+            }}
+        >
+            <div
+                style={{
+                    display: "inline-block",
+                    width: "90vw",
+                    margin: "5vw",
+                }}
+            >
+                <FullCalendar
+                    headerToolbar={{
+                        start: "title",
+                        center: "",
+                        end: "today",
+                    }}
+                    plugins={[dayGridPlugin, interactionPlugin]}
+                    height="80vh"
+                    initialView={initialView}
+                    initialEvents={INITIAL_EVENTS}
+                    locales={allLocales}
+                    locale="ja"
+                    titleFormat={{
+                        month: "short",
+                        year: "numeric",
+                    }}
+                    dayCellContent={(e) => {
+                        return e.dayNumberText.replace("日", "");
+                    }}
+                />
+            </div>
+        </div>
+    );
 }
+
 export default LoggedIn;
--- webapp/src/components/Header.tsx
+++ webapp/src/components/Header.tsx
@@ -3,8 +3,6 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import { isLoggedIn, COLOR } from "../App";
 import liff from "@line/liff";
 
@@ -42,9 +40,6 @@ function Header() {
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" sx={{ mr: 2 }}>
-                        <MenuIcon />
-                    </IconButton>
                     <Typography
                         variant="h6"
                         component="div"
