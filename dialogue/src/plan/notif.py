630444dc6d2783d14100b090d8eaf56ef6a63ba3
whtsht <whiteshirt0079@gmail.com>
2023-05-19 16:02:32 +0900

    最新の予定を記録

--- dialogue/src/plan/notif.py
+++ dialogue/src/plan/notif.py
@@ -4,27 +4,28 @@ from linebot import LineBotApi
 from linebot.models import (
     TextMessage,
 )
+from src.db_models import Plan
 from src.secret import CHANNEL_ACCESS_TOKEN
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 sched = APScheduler()
 
-# lineID => 予定名を検索
-latest_plan: dict[str, str] = {}
+# lineID => 予定を検索
+latest_plan: dict[str, Plan] = {}
 
 
 def gen_id(line_id: str, title: str, date: datetime) -> str:
     return line_id + "_" + title + "_" + str(date)
 
 
-def add_notification(line_id: str, title: str, date: datetime):
+def add_notification(line_id: str, plan: Plan):
     sched.add_job(
-        gen_id(line_id, title, date),
+        gen_id(line_id, plan.title, plan.notif_time),
         send_notification,
         trigger="date",
-        run_date=date,
-        args=[line_id, title],
+        run_date=plan.date,
+        args=[line_id, plan],
     )
 
 
@@ -40,8 +41,8 @@ def snooze(line_id: str):
     pass
 
 
-def send_notification(line_id: str, title: str):
+def send_notification(line_id: str, plan: Plan):
     # Lineにメッセージを送信
-    line_bot_api.push_message(line_id, TextMessage(text="「" + title + "」の時間です"))
+    line_bot_api.push_message(line_id, TextMessage(text="「" + plan.title + "」の時間です"))
     # 予定名を記録
-    latest_plan[line_id] = title
+    latest_plan[line_id] = plan
